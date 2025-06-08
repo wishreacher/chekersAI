b0f27a78883705c08daa0b9c63c0aef5b145a5f9
klirik <wishreacher@gmail.com>
2025-06-08 20:43:33 +0300

    Init

---
+++ chekersAI/chekersAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  chekersAIApp.swift
+//  chekersAI
+//
+//  Created by Володимир on 08.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct chekersAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
