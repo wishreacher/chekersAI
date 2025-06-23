46cf9ee711fdc569998780c80ffd908e1d049b1c
klirik <wishreacher@gmail.com>
2025-06-23 22:08:19 +0300

    Best move to camera detection

--- chekersAI/Views/CameraView.swift
+++ chekersAI/Views/CameraView.swift
@@ -5,10 +5,28 @@ struct CameraView: View {
     
     var body: some View {
         VStack(spacing: 0) {
+            HStack {
+                Text("Best Move: 🤩")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding(.vertical, 15)
+            
+            Text(analyzer.moveToString())
+                .monospaced()
+                .frame(height: 20)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.blue.opacity(0.4))
+                .clipShape(RoundedRectangle(cornerRadius: 15))
+            
+            Spacer()
+            
             Text("Наведіть камеру на дошку")
                 .font(.headline)
             
-            Spacer()
+            
             analyzer.arViewRep
                 .frame(width: 300, height: 300)
                 .padding(.bottom, 70)
